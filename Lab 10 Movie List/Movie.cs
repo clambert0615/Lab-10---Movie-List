ca3b954b1ed64d858027c4b4140ffa04ecc94996
Christina Lambert <clambert0615@gmail.com>
2020-04-30 19:28:10 -0400

    Initial commit for lab 10 - movie list

---
+++ Lab 10 Movie List/Movie.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_10_Movie_List
+{
+    class Movie
+    {
+        //private fields
+        private string title;
+        private string category;
+        private int categoryNumber;
+
+        //public properties
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
+        }
+        public int CategoryNumber
+        {
+            get { return categoryNumber; }
+            set { categoryNumber = value; }
+        }
+
+        // constructors
+
+        public Movie(string _title, string _category, int _categoryNumber)
+        {
+            title = _title;
+            category = _category;
+            categoryNumber = _categoryNumber;
+        }
+
+        public Movie()
+        {
+
+        }
+
+        //Methods
+
+        public static void FillListMovies(List <Movie> movies)
+        {
+            movies.Add(new Movie("Black Panther", "drama", 1));
+            movies.Add(new Movie("Citizen Kane", "drama", 1));
+            movies.Add(new Movie("Frozen", "animated", 2));
+            movies.Add(new Movie("Finding Nemo", "animated", 2));
+            movies.Add(new Movie("Toy Story", "animated", 2));
+            movies.Add(new Movie("Pet Sematary", "horror", 3));
+            movies.Add(new Movie("It", "horror", 3));
+            movies.Add(new Movie("The Conjuring", "horror", 3));
+            movies.Add(new Movie("Star Wars: A New Hope", "sci-fi", 4));
+            movies.Add(new Movie("Star Wars: The Empire Strikes Back", "sci-fi", 4));
+            movies.Sort((x,y) => x.Title.CompareTo(y.Title));
+        }
+    }
+}
---
+++ Lab 10 Movie List/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_10_Movie_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepGoing = true;
+            while (keepGoing)
+            {
+                List<Movie> movies = new List<Movie>();
+                Movie.FillListMovies(movies);
+
+                string userChoice = GetSearchChoice("Would you like to select by category or number?  Type cateogy or number");
+              
+                if (userChoice == "category")
+                {
+                    string categoryChoice = GetUserInput("Which category would you like to see? drama, sci-fi, animated, or horror");
+
+                    DisplayMovies(categoryChoice, movies);
+                }
+                else if (userChoice == "number")
+                {
+                    int numberChoice = GetCategoryNumber("Which category would you like to see more about? Enter a number \n 1. drama \n 2. animated \n 3. horror \n 4. sci-fi");
+                    DisplayMoviebyNumber(numberChoice, movies);
+                }
+
+
+                keepGoing = KeepAsking(); 
+            }
+        }
+
+        public static string GetUserInput(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                string input = Console.ReadLine().ToLower().Trim();
+                if (!string.IsNullOrEmpty(input) && (input == "horror" || input == "animated" || input == "sci-fi" || input == "drama"))
+                {
+                    return input;
+                }
+                else
+                {
+                    Console.WriteLine("That was invalid input, try again!");
+                    continue;
+                }
+
+            }
+        }
+
+        public static int GetCategoryNumber(string message)
+        {
+            while (true)
+            {
+                int number = 0;
+
+                try
+                {
+
+                    Console.WriteLine(message);
+                    number = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Not a number. Try again");
+                    continue;
+                }
+                if (number >= 1 && number <= 4)
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.WriteLine("Number is not between 1 and 4. Try again!");
+                    continue;
+                }
+            }
+         }
+
+
+        public static string GetSearchChoice(string message)
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                string choice = Console.ReadLine().ToLower().Trim();
+                if (!String.IsNullOrEmpty(choice) && choice == "number" || choice == "category")
+                {
+                    return choice;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Try again");
+                    continue;
+                }
+            }
+        }
+
+        public static void DisplayMovies(string userChoice, List<Movie> movies)
+        {
+            foreach (Movie m in movies)
+            {
+                if (m.Category == userChoice)
+                {
+                    
+                    Console.WriteLine(m.Title);
+                }
+            }
+
+        }
+        public static void DisplayMoviebyNumber(int numberChoice, List<Movie> movies)
+        {
+            foreach (Movie m in movies)
+            {
+                if(m.CategoryNumber == numberChoice)
+                {
+                    Console.WriteLine(m.Title);
+                }
+            }
+        }
+        public static bool KeepAsking()
+        {
+            while (true)
+            {
+                Console.WriteLine("Would you like to continue? y or n");
+                string decision = Console.ReadLine().ToLower().Trim();
+                if (decision == "y")
+                {
+                    return true;
+                }
+                if (decision == "n")
+                {
+                    Console.WriteLine("Have a great day! Goodbye");
+                    return false;
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input.  Try again!");
+                    continue;
+                }
+            }
+        }
+
+    }
+}
